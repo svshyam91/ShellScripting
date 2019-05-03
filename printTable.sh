ed95eaada963d84b082fe60db2009958e9bc435e
Shyam Kumar <svshyam97@gmail.com>
2019-05-03 23:10:12 +0530

    Initial Commit

---
+++ printTable.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Program to print the table of user defined number
+
+# Taking input from user
+read -p "Enter any number: " number
+
+for (( i = 1; i <= 10; i++ )); do
+	echo "$number * $i = "$[$number*$i]
+done
+
