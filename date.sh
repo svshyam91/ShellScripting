fe5d4c1ac1bfd471ea905abe108539f329b2391b
Shyam Kumar <svshyam111@gmail.com>
2019-05-15 10:06:28 +0530

    Added customized date and time

---
+++ date.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Program to print date, time and month according to 
+# user preference
+
+while [[ true ]]; do
+	echo "Program to get Customized Date and Time.."
+	echo "1. Get Date" 
+	echo "2. Get Time"
+	echo "3. Get Year"
+	echo "4. Exit"
+	echo "Choose the appropriate option: "; read op
+	case $op in
+		1)
+		date +"%D"
+			;;
+		2)
+		date +"%X"
+			;;
+		3)
+		date +"%Y"
+			;;
+		4)
+		exit
+			;;
+	esac
+done
