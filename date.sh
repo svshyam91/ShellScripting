0551eb3538bbaaaaf557ffe8a7c4f3490fc6c92f
Shyam Kumar <svshyam111@gmail.com>
2019-05-19 11:12:49 +0530

    Customized date.sh file

--- date.sh
+++ date.sh
@@ -3,25 +3,43 @@
 # Program to print date, time and month according to 
 # user preference
 
-while [[ true ]]; do
+
+
+# while [[ : ]]; do
+# 	d=$(date +"%M")
+# 	if [[ $d == "33" ]]; then
+# 		execute 
+# 	fi
+# 	exit
+# done
+
+
+
+execute() {
 	echo "Program to get Customized Date and Time.."
-	echo "1. Get Date" 
-	echo "2. Get Time"
-	echo "3. Get Year"
-	echo "4. Exit"
-	echo "Choose the appropriate option: "; read op
-	case $op in
-		1)
-		date +"%D"
-			;;
-		2)
-		date +"%X"
-			;;
-		3)
-		date +"%Y"
-			;;
-		4)
-		exit
-			;;
-	esac
-done
+	while [[ true ]]; do
+		echo "1. Get Date" 
+		echo "2. Get Time"
+		echo "3. Get Year"
+		echo "4. Exit"
+		echo "Choose the appropriate option: "; read op
+		case $op in
+			1)
+			date +"%D"
+				;;
+			2)
+			date +"%X"
+				;;
+			3)
+			date +"%Y"
+				;;
+			4)
+			exit
+				;;
+		esac
+	done
+
+}
+
+
+execute
