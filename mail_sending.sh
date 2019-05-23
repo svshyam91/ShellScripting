25733e121636e211727cb6f0db12645eb770403a
Shyam Kumar <svshyam111@gmail.com>
2019-05-23 23:53:03 +0530

    Added different options for attaching files

--- mail_sending.sh
+++ mail_sending.sh
@@ -5,12 +5,22 @@
 read -p "Enter recepient mail address: " email
 read -p "Enter subject for Email: " sub
 read -p "Enter content for Email: " cont
+read -p "Want to attach file (y/n) ?" op
+path=""
+if [[ $op == "y" ]]; then
+	read -p "Enter the path of the file: " path
+	# Mutt command for sending mail
 
-# Mutt command for sending mail
-echo "$cont" | mutt -s "$sub" $email
+	echo "$cont" | mutt -s "$sub" $email -a $path
+else
+	# Mutt command for sending mail
+
+	echo "$cont" | mutt -s "$sub" $email
+fi
 
 if [[ $? -eq 0 ]]; then
 	echo "Mail sent successfully."
 else
 	echo "Something went wrong..."
+	return 1
 fi
