5e124d89549fddf54c868505309eee8730142bb6
Shyam Kumar <svshyam111@gmail.com>
2020-03-18 17:14:55 +0530

    Removed my name in greeting and exported DISPLAY and DBUS_SESSION_BUS variable to show notification

--- customGreetings.sh
+++ customGreetings.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# Exporting Environment variables for sending notification
+export DISPLAY=:0.0
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
+
 # Get current hour
 hour=$(date +"%H")
 
@@ -7,18 +11,19 @@ hour=$(date +"%H")
 min=$(date +"%M")
 
 if [[ $hour -gt 5 && $hour -lt 12 ]]; then
-	mssg="Good Morning!! Shyam.."
+	mssg="Good Morning!! "
 	img="/home/svshyam97/Programming/ShellScripting/sun.png"
 elif [[ $hour -gt 12 && $hour -lt 16 ]]; then
-	mssg="Good Afternoon!! Shyam.."
+	mssg="Good Afternoon!! "
 	img="/home/svshyam97/Programming/ShellScripting/sun.png"
 elif [[ $hour -ge 16 && $hour -lt 21 ]]; then
-	mssg="Good Evening!! Shyam.."
+	mssg="Good Evening!! "
 	img="/home/svshyam97/Programming/ShellScripting/moon.png"
 else
-	mssg="Good Night!! Shyam.."
+	mssg="Good Night!! "
+	img="/home/svshyam97/Programming/ShellScripting/moon.png"
 fi
 
 # echo "$mssg"
 # Sending Notification instead of sending message to the terminal
-notify-send -i $(echo $img) "Hello !" "$mssg"
+notify-send -i $(echo $img) "$mssg" "Shyam K."
